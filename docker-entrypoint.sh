ccd617267e8704ad4d33b3a0c67754f030a2b635
Thomas pre <thomas.prevet@gmail.com>
2019-01-26 12:10:22 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 : "${RT_WEB_PORT:=80}"
 
 sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4/etc/RT_SiteConfig.pm
 
 exec "$@"
