5d6b1a3b938c9702316d286feda4b7e391c44526
Thomas pre <thomas.prevet@gmail.com>
2019-01-26 14:38:07 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,10 +7,14 @@ set -euo pipefail
 sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4/etc/RT_SiteConfig.pm
 sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4/etc/RT_SiteConfig.pm
 
+if [ ! -d /opt/rt4_data/etc ]; then
+
 cd /opt
 cp rt4/* rt4_data/
 rm -rf rt4
 ln -s /opt/rt4_data rt4
+
+fi
 
 chown -R www-data:www-data /opt/rt4_data/
 
