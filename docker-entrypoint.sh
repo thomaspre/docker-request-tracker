a24f72ca700b9b379747f5ed68b095247de4fa5a
Thomas pre <thomas.prevet@gmail.com>
2019-01-26 15:14:11 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,13 +6,21 @@ ls -al /opt/
 
 if [ ! -f "/opt/rt4_data/etc/RT_SiteConfig.pm" ]; then
   cd /opt
-  echo "rt4:"
+
   ls rt4/
+  
   cp -r rt4/* rt4_data/
-  echo "rt4_data:"
+  
   ls rt4_data/
+  
+  /etc/init.d/httpd stop
+  /etc/init.d/postfix stop
+
   rm -rf rt4
   ln -s /opt/rt4_data rt4
+  
+  /etc/init.d/httpd start
+  /etc/init.d/postfix start
 fi
 
 ls -al /opt/
@@ -24,6 +32,6 @@ RT_WEB_URL="${RT_WEB_URL//\//\\/}"
 sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4_data/etc/RT_SiteConfig.pm
 sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4_data/etc/RT_SiteConfig.pm
 
-chown -R www-data:www-data /opt/rt4_data/
+chown -R www-data:www-data /opt/
 
 exec "$@"
