8af8dd364c4052a4f3953a7de461ae6d980990c5
Thomas pre <thomas.prevet@gmail.com>
2019-01-26 14:40:47 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,19 +2,17 @@
 
 set -euo pipefail
 
+if [ ! -f /opt/rt4/etc/RT_SiteConfig.pm ]; then
+  cd /opt
+  cp rt4/* rt4_data/
+  rm -rf rt4
+  ln -s /opt/rt4_data rt4
+fi
+
 : "${RT_WEB_PORT:=80}"
 
-sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4/etc/RT_SiteConfig.pm
-sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4/etc/RT_SiteConfig.pm
-
-if [ ! -d /opt/rt4_data/etc ]; then
-
-cd /opt
-cp rt4/* rt4_data/
-rm -rf rt4
-ln -s /opt/rt4_data rt4
-
-fi
+sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4_data/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4_data/etc/RT_SiteConfig.pm
 
 chown -R www-data:www-data /opt/rt4_data/
 
