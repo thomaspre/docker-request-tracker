2da844643bfe64c74243a48edbf740c709dfd479
Thomas pre <thomas.prevet@gmail.com>
2019-01-27 14:15:29 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,28 +2,15 @@
 
 set -euo pipefail
 
-ls -al /opt/
 cd /opt
 
-if [ ! -d "/opt/rt4_data/var/" ]; then
-
-  echo "---- ls rt4"
-  ls -al rt4/
-  
+if [ ! -d "/opt/rt4_data/var/" ]; then  
   mkdir rt4_data/var/
-  cp -r rt4/var/* rt4_data/var/
-  
-  echo "---- ls rt4_data"
-  ls -al rt4_data/
-  ls -al rt4_data/var/
-  
+  cp -r rt4/var/* rt4_data/var/ 
 fi
 
 rm -rf rt4/var
 ln -s /opt/rt4_data/var rt4/var
-  
-echo "---- ls opt"
-ls -al /opt/rt4/
 
 : "${RT_WEB_PORT:=80}"
 
@@ -34,6 +21,9 @@ sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4/etc/RT_SiteConfig.pm
 
 chown -R www-data:www-data /opt/rt4_data
 
-apache2-foreground
+echo "---- ls opt"
+ls -al /opt/rt4/
+ls -al /opt/rt4_data/
+ls -al /opt/rt4_data/var/
 
 exec "$@"
