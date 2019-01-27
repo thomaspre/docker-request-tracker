52d648694ae512c7a4f8bdfd2a4d89033e893e55
Thomas pre <thomas.prevet@gmail.com>
2019-01-27 14:06:31 +0100

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,34 +3,34 @@
 set -euo pipefail
 
 ls -al /opt/
+cd /opt
 
-if [ ! -f "/opt/rt4_data/etc/RT_SiteConfig.pm" ]; then
-  cd /opt
+if [ ! -d "/opt/rt4_data/var/" ]; then
 
   echo "---- ls rt4"
-  ls rt4/
+  ls -al rt4/
   
-  cp -r rt4/* rt4_data/
+  mkdir rt4_data/var/
+  cp -r rt4/var/* rt4_data/var/
   
   echo "---- ls rt4_data"
-  ls rt4_data/
+  ls -al rt4_data/
+  ls -al rt4_data/var/
   
 fi
 
-sleep 120
-
-rm -rf /opt/rt4
-ln -s /opt/rt4_data rt4
+rm -rf rt4/var
+ln -s /opt/rt4_data/var rt4/var
   
 echo "---- ls opt"
-ls -al /opt/
+ls -al /opt/rt4/
 
 : "${RT_WEB_PORT:=80}"
 
 RT_WEB_URL="${RT_WEB_URL//\//\\/}"
 
-sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4_data/etc/RT_SiteConfig.pm
-sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4_data/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_PORT/$RT_WEB_PORT/" /opt/rt4/etc/RT_SiteConfig.pm
+sed -i "s/RT_WEB_URL/$RT_WEB_URL/" /opt/rt4/etc/RT_SiteConfig.pm
 
 chown -R www-data:www-data /opt/rt4_data
 
